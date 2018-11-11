8d02d90baa1325394b8af246a53938370f616b77
shih14 <shihan.rock96@gmail.com>
2018-11-11 17:13:57 +0800

    TopRSBC

--- TopRSBC.py
+++ TopRSBC.py
@@ -5,19 +5,28 @@ from sklearn.model_selection import train_test_split
 
 
 def data_pre(data):
-    data_p = data - tf.reduce_mean(data, 0)
+    mean = tf.reduce_mean(data, axis=1)
+    data_p = data - mean
+    # print(data.shape, mean.shape, data_p.shape)
     return data_p
 
 
 def triplet_loss(anchor, positive, negative, n):
     # anchor p*d, positive p*d, negative p*d
     with tf.variable_scope('triplet_loss'):
-        p = tf.shape(anchor)[0]
-        pos_dist = tf.norm(tf.subtract(anchor, positive), ord=1)
-        neg_dist = tf.norm(tf.subtract(anchor, negative), ord=1)
-        basic_loss = tf.subtract(pos_dist, neg_dist)
-        sum_loss = tf.reduce_sum(tf.nn.sigmoid(basic_loss))
+        p = anchor.shape[1]
+        # print("p{}".format(p))
+        pos = anchor - positive
+        neg = anchor - negative
+        print("pos{}".format(pos.shape))
+        pos_dist = tf.norm(pos, ord=1, axis=2)
+        neg_dist = tf.norm(neg, ord=1, axis=2)
+        basic_loss = pos_dist - neg_dist
+        sigmoid_basic_loss = tf.nn.sigmoid(basic_loss)
+        print(anchor.shape, pos_dist.shape, basic_loss.shape, sigmoid_basic_loss.shape, "loss")
+        sum_loss = tf.reduce_sum(sigmoid_basic_loss)
         loss = tf.math.log(1 + tf.math.floor(n / p) * sum_loss)
+        print(loss.shape)
     return loss
 
 
@@ -25,8 +34,10 @@ def encoder(placeholder_x, bits):
     with tf.variable_scope("encoder"):
         fc1 = tf.layers.dense(placeholder_x,
                               bits,
-                              activation=tf.nn.tanh)
-    print(placeholder_x.shape, fc1.shape)
+                              activation=tf.nn.tanh,
+                              kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                              kernel_regularizer=tf.contrib.layers.l2_regularizer(0.001))
+    # print(placeholder_x.shape, fc1.shape)
     return fc1
 
 
@@ -35,19 +46,22 @@ def get_loss(placeholder_x, bits, p, n):
     data_p = data_pre(placeholder_x)  # pre
     encoded = encoder(data_p, bits)
     anchor = encoded[:, 0, :]
-    print(encoded.shape)
-    anchors = tf.reshape(tf.tile(anchor, [1, p]), [p, -1])
+    print("anchor{}".format(anchor.shape))
+    dim = anchor.shape[1]
+    # print(dim, "#")
+    anchors = tf.reshape(tf.tile(anchor, [1, p]), [-1, p, dim])
     positive = encoded[:, 1, :]
-    positives = tf.reshape(tf.tile(positive, [1, p]), [p, -1])
+    positives = tf.reshape(tf.tile(positive, [1, p]), [-1, p, dim])
     negatives = encoded[:, 2:, :]
-    loss = triplet_loss(anchors, positives, negatives, n)
+    print(anchors.shape, negatives.shape, positives.shape)
+    loss = triplet_loss(anchors, positives, negatives, n) + tf.losses.get_regularization_loss()
     return loss
 
 
-def train_model(train_x, placeholder_x, placeholder_n, bits, learning_rate):
-    num = np.shape(train_x)[0]  # sample number
-    Num_p = 200
-    max_class = max(train_x[:, -1]) + 1  # 0 1 2...
+def train_model(train_x, placeholder_x, placeholder_n, bits, learning_rate, Num_p):
+    # num = np.shape(train_x)[0]  # sample number
+    max_class = int(max(train_x[:, -1])) + 1  # 0 1 2...
+    print("max{}".format(max_class))
     num_iterations = 1000
     loss = get_loss(placeholder_x, bits, Num_p, placeholder_n)
     model_saver = tf.train.Saver(max_to_keep=None)
@@ -70,9 +84,10 @@ def train_model(train_x, placeholder_x, placeholder_n, bits, learning_rate):
             Num_n = len(negatives)
             slices = np.random.randint(Num_n, size=Num_p)
             N = negatives[slices]
-            print(A.shape, P.shape, N.shape)
+            print(A.shape, P.shape, N.shape, Num_n)
             x_batch = np.concatenate((A, P, N), axis=0)[:, :-1]
             x_batch = x_batch[np.newaxis, :]
+            print(x_batch.shape)
             feed_dict = {placeholder_x: x_batch, placeholder_n: np.array([Num_n])[np.newaxis, :]}
             _, loss_eval = sess.run([opt, loss], feed_dict=feed_dict)
             print("Batch {} finished. loss={}".format(n_pass, loss_eval))
@@ -89,14 +104,14 @@ def main():
     data = sio.loadmat("./datasets/eeg.mat")["data"]
     data[np.isnan(data)] = 0
     train_x, test_x = train_test_split(data, test_size=0.2)
-    input_dim = np.shape(train_x)[1] - 1
-    output_dim = 10 # bits
-    Num_p = 200
+    input_dim = np.shape(train_x)[1] - 1  # features+label
+    output_dim = 64  # bits
+    Num_p = 1800
     learning_rate = 0.01
     with tf.variable_scope("placeholders"):
         series_var = tf.placeholder(tf.float64, shape=(None, Num_p+2, input_dim), name="series")
         n_var = tf.placeholder(tf.int32, shape=(None, 1), name="N")
-    train_model(train_x, series_var, n_var, output_dim, learning_rate)
+    train_model(train_x, series_var, n_var, output_dim, learning_rate, Num_p)
 
 
 if __name__ == "__main__":
